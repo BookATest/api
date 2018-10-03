e2a339dbc6c625dcffef3d65b4be220612b27073
Matt Inamdar <matt@ayup.agency>
2018-10-03 14:55:39 +0100

    added placeholder test cases for reports and report schedules

---
+++ tests/Feature/ReportSchedulesTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ReportSchedulesTest extends TestCase
+{
+    public function test_placeholder()
+    {
+        $this->markTestIncomplete('Need to figure out reports needed first');
+    }
+}
---
+++ tests/Feature/ReportsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ReportsTest extends TestCase
+{
+    public function test_placeholder()
+    {
+        $this->markTestIncomplete('Need to figure out reports needed first');
+    }
+}
