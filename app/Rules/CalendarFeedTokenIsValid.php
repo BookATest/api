1f1c5dd7df2e0e9e1e5e557fca95f9a239a67583
Matt Inamdar <matt@ayup.agency>
2018-10-19 13:41:09 +0100

    added handler for exceptions on generated file routes
    This is to prevent Laravel automatically redirecting upon validation failures.

--- app/Rules/CalendarFeedTokenIsValid.php
+++ app/Rules/CalendarFeedTokenIsValid.php
@@ -30,6 +30,6 @@ class CalendarFeedTokenIsValid implements Rule
      */
     public function message()
     {
-        return 'The validation error message.';
+        return 'The :attribute is invalid.';
     }
 }
