92a5962f160f647e22efded7e0e7d7026ed1c0e2
Matt Inamdar <matt@ayup.agency>
2018-10-24 10:18:57 +0100

    added load balancer security group

--- aws/cloudformation.py
+++ aws/cloudformation.py
@@ -1,70 +1,31 @@
 # Converted from EC2InstanceSample.template located at:
 # http://aws.amazon.com/cloudformation/aws-cloudformation-templates/
 
-from troposphere import Base64, FindInMap, GetAtt
-from troposphere import Parameter, Output, Ref, Template
-import troposphere.ec2 as ec2
+from troposphere import ec2, Template
 
+template = Template('Create the infrastructure needed to run the Book A Test web app')
+template.add_version('2010-09-09')
 
-template = Template()
+# Create the security groups.
 
-keyname_param = template.add_parameter(Parameter(
-    "KeyName",
-    Description="Name of an existing EC2 KeyPair to enable SSH "
-                "access to the instance",
-    Type="String",
-))
-
-template.add_mapping('RegionMap', {
-    "us-east-1": {"AMI": "ami-7f418316"},
-    "us-west-1": {"AMI": "ami-951945d0"},
-    "us-west-2": {"AMI": "ami-16fd7026"},
-    "eu-west-1": {"AMI": "ami-24506250"},
-    "sa-east-1": {"AMI": "ami-3e3be423"},
-    "ap-southeast-1": {"AMI": "ami-74dda626"},
-    "ap-northeast-1": {"AMI": "ami-dcfa4edd"}
-})
-
-ec2_instance = template.add_resource(ec2.Instance(
-    "Ec2Instance",
-    ImageId=FindInMap("RegionMap", Ref("AWS::Region"), "AMI"),
-    InstanceType="t1.micro",
-    KeyName=Ref(keyname_param),
-    SecurityGroups=["default"],
-    UserData=Base64("80")
-))
-
-template.add_output([
-    Output(
-        "InstanceId",
-        Description="InstanceId of the newly created EC2 instance",
-        Value=Ref(ec2_instance),
-    ),
-    Output(
-        "AZ",
-        Description="Availability Zone of the newly created EC2 instance",
-        Value=GetAtt(ec2_instance, "AvailabilityZone"),
-    ),
-    Output(
-        "PublicIP",
-        Description="Public IP address of the newly created EC2 instance",
-        Value=GetAtt(ec2_instance, "PublicIp"),
-    ),
-    Output(
-        "PrivateIP",
-        Description="Private IP address of the newly created EC2 instance",
-        Value=GetAtt(ec2_instance, "PrivateIp"),
-    ),
-    Output(
-        "PublicDNS",
-        Description="Public DNSName of the newly created EC2 instance",
-        Value=GetAtt(ec2_instance, "PublicDnsName"),
-    ),
-    Output(
-        "PrivateDNS",
-        Description="Private DNSName of the newly created EC2 instance",
-        Value=GetAtt(ec2_instance, "PrivateDnsName"),
-    ),
-])
+load_balancer_security_group = ec2.SecurityGroup(
+    "LoadBalancerSecurityGroup",
+    GroupDescription='For connecting to the API load balancer',
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol='tcp',
+            FromPort='80',
+            ToPort='80',
+            CidrIp='0.0.0.0/0'
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol='tcp',
+            FromPort='443',
+            ToPort='443',
+            CidrIp='0.0.0.0/0'
+        )
+    ]
+)
+template.add_resource(load_balancer_security_group)
 
 print(template.to_json())
