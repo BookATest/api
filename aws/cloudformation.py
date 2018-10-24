0ced4fa6f3a2f9243f7920c6c2db6167fdb0abe9
Matt Inamdar <matt@ayup.agency>
2018-10-24 10:44:40 +0100

    added api security group

--- aws/cloudformation.py
+++ aws/cloudformation.py
@@ -1,24 +1,25 @@
 # Converted from EC2InstanceSample.template located at:
 # http://aws.amazon.com/cloudformation/aws-cloudformation-templates/
 
-from troposphere import ec2, Template
+from troposphere import ec2, Ref, Template
 
 template = Template('Create the infrastructure needed to run the Book A Test web app')
 template.add_version('2010-09-09')
 
 # Create the security groups.
-
 load_balancer_security_group = ec2.SecurityGroup(
-    "LoadBalancerSecurityGroup",
+    'LoadBalancerSecurityGroup',
     GroupDescription='For connecting to the API load balancer',
     SecurityGroupIngress=[
         ec2.SecurityGroupRule(
+            Description='HTTP access from the public',
             IpProtocol='tcp',
             FromPort='80',
             ToPort='80',
             CidrIp='0.0.0.0/0'
         ),
         ec2.SecurityGroupRule(
+            Description='HTTPS access from the public',
             IpProtocol='tcp',
             FromPort='443',
             ToPort='443',
@@ -27,5 +28,20 @@ load_balancer_security_group = ec2.SecurityGroup(
     ]
 )
 template.add_resource(load_balancer_security_group)
+
+api_security_group = ec2.SecurityGroup(
+    'ApiSecurityGroup',
+    GroupDescription='For connecting to the API containers',
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            Description='Full access from the load balancer',
+            IpProtocol='tcp',
+            FromPort='0',
+            ToPort='65535',
+            SourceSecurityGroupName=Ref(load_balancer_security_group)
+        )
+    ]
+)
+template.add_resource(api_security_group)
 
 print(template.to_json())
