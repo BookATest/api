8021e4e650a531a569715003326ebde1d5af0c93
Matt Inamdar <matt@ayup.agency>
2018-11-13 11:02:12 +0000

    added users seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Clinic;
+use App\Models\User;
 use App\Support\Coordinate;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $clinics = $this->createClinics(50);
+        $users = $this->createUsers(200);
     }
 
     /**
@@ -58,5 +60,44 @@ class DatabaseSeeder extends Seeder
             mt_rand($minLat * $factor, $maxLat * $factor) / $factor,
             mt_rand($minLon * $factor, $maxLon * $factor) / $factor
         );
+    }
+
+    /**
+     * @param int $count
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    protected function createUsers(int $count): Collection
+    {
+        $users = new Collection();
+
+        foreach (range(1, $count) as $index) {
+            /** @var \App\Models\User $user */
+            $user = factory(User::class)->create();
+
+            /** @var \App\Models\Clinic $clinic */
+            $clinic = Clinic::query()->inRandomOrder()->firstOrFail();
+
+            // Generate a random user role.
+            $organisationAdmin = 1;
+            $clinicAdmin = 2;
+            $communityWorker = 3;
+            $seed = mt_rand(1, 3);
+
+            switch ($seed) {
+                case $organisationAdmin:
+                    $user->makeOrganisationAdmin();
+                    break;
+                case $clinicAdmin:
+                    $user->makeClinicAdmin($clinic);
+                    break;
+                case $communityWorker:
+                    $user->makeCommunityWorker($clinic);
+                    break;
+            }
+
+            $users->push($user);
+        }
+
+        return $users;
     }
 }
