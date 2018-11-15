70a271fb82f5a6a4de2a6924297a9802f8a4b0e2
Matt Inamdar <matt@ayup.agency>
2018-11-15 16:21:22 +0000

    added command to clear cached geocode results

---
+++ app/Console/Commands/Bat/ClearCachedGeocodeResultsCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands\Bat;
+
+use App\Models\CachedGeocodeResult;
+use Illuminate\Console\Command;
+
+class ClearCachedGeocodeResultsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bat:clear-cached-geocode-results';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clears the cached geocode results';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        CachedGeocodeResult::query()->truncate();
+    }
+}
