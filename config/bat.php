8cab8a9303e4506cb88365c50bca084b2ff8e551
Matt Inamdar <matt@ayup.agency>
2018-11-26 17:45:50 +0000

    allowd max_service_user_token_attempts to be specified by environment

--- config/bat.php
+++ config/bat.php
@@ -4,6 +4,6 @@ return [
 
     'otp_enabled' => env('OTP_ENABLED', true),
 
-    'max_service_user_token_attempts' => 3,
+    'max_service_user_token_attempts' => env('MAX_SERVICE_USER_TOKEN_ATTEMPTS', 3),
 
 ];
