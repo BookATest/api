d6cef9125181b8cddbdeb3a76abc3ef8680ba118
Matthew Inamdar <matthew.inamdar@goldspecdigital.com>
2018-11-29 13:42:04 +0000

    updated guard when logging out the user

--- app/Http/Controllers/V1/User/SessionController.php
+++ app/Http/Controllers/V1/User/SessionController.php
@@ -8,13 +8,21 @@ use Illuminate\Support\Facades\Auth;
 class SessionController extends Controller
 {
     /**
+     * SessionController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    /**
      * Remove the specified resource from storage.
      *
      * @return \Illuminate\Http\Response
      */
     public function destroy()
     {
-        Auth::user()->clearSessions();
+        Auth::guard('api')->user()->clearSessions();
 
         return response()->json(['message' => 'All your sessions have been cleared.']);
     }
