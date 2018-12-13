f4bb863f231a8ce5cc40aa6692c1c278f6fad850
Matt Inamdar <matt@ayup.agency>
2018-12-13 13:29:34 +0000

    added updated settings response

--- app/Docs/Resources/SettingResource.php
+++ app/Docs/Resources/SettingResource.php
@@ -15,13 +15,79 @@ class SettingResource extends BaseResource
             Schema::integer('default_appointment_booking_threshold'),
             Schema::integer('default_appointment_duration'),
             Schema::object('language')->properties(
-                Schema::string('booking_questions_help_text'),
-                Schema::string('booking_notification_help_text'),
-                Schema::string('booking_enter_details_help_text'),
-                Schema::string('booking_find_location_help_text'),
-                Schema::string('booking_appointment_overview_help_text')
+                Schema::object('home')->properties(
+                    Schema::string('title'),
+                    Schema::string('content')
+                ),
+                Schema::object('make-booking')->properties(
+                    Schema::object('clinics')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('consent')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('location')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('overview')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('questions')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('appointments')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('confirmation')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('introduction')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('user-details')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    )
+                ),
+                Schema::object('list-bookings')->properties(
+                    Schema::object('token')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('cancel')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('cancelled')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('access-code')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    ),
+                    Schema::object('appointments')->properties(
+                        Schema::string('title'),
+                        Schema::string('content'),
+                        Schema::string('disclaimer')
+                    ),
+                    Schema::object('token-expired')->properties(
+                        Schema::string('title'),
+                        Schema::string('content')
+                    )
+                )
             ),
             Schema::string('name'),
+            Schema::string('email'),
+            Schema::string('phone'),
             Schema::string('primary_colour'),
             Schema::string('secondary_colour')
         );
