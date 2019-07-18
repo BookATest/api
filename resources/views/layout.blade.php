9c44371140d70e57de2150dd8bb826d62cda0330
Matt Inamdar <matt@ayup.agency>
2019-07-18 16:42:48 +0100

    feature/develop-script (#131)
    * Simplified develop script and Docker config
    
    * Using default Swagger UI theme
    
    * Stopped mysql before starting containers
    
    * Using new container name for CI

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>@yield('title', config('app.name', 'Laravel'))</title>
+
+  <!-- Styles -->
+  <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
+  @yield('css')
+</head>
+<body>
+  <div class="base-layout">
+
+    {{-- Header --}}
+    <div class="base-layout__header base-layout__header--login">
+      <div class="header">
+        <a href="{{ route('home') }}">
+          <img src="{{ asset('img/logo-bookatest.png') }}" alt="{{ config('app.name') }} logo">
+        </a>
+      </div>
+    </div>
+
+    {{-- Main content --}}
+    <div class="base-layout__main base-layout__main--no-sidebar">
+      <div class="main">
+        @yield('content')
+      </div>
+    </div>
+  </div>
+
+  @yield('js')
+</body>
+</html>
