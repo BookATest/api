37bfd7d1543fd4c8ffc9b94f5c9d87d64ad8ef86
Matt Inamdar <matt@ayup.agency>
2019-09-24 11:12:21 +0100

    Set tokens to expire at midnight

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Date;
 use Laravel\Passport\Passport;
 use Laravel\Passport\RouteRegistrar;
 
@@ -38,5 +39,6 @@ class AuthServiceProvider extends ServiceProvider
             $router->forAuthorization();
         });
         Passport::enableImplicitGrant();
+        Passport::tokensExpireIn(Date::today()->endOfDay());
     }
 }
